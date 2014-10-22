c5e445398cff1e02c56fec13c3456c47498ebecc
Emory Griffith <emorygriffith91@gmail.com>
2014-10-22 13:17:05 -0400

    Added players and enemies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,46 @@
 var Player = function (options) {
   this.name = options.name;
   this.health = options.health;
+  this.attack = function(warrier){
+    warrier.health = options.health -10;
+  };
 
 };
 
 var Enemy = function (options) {
   this.name = options.name;
   this.health = options.health;
-
-}
+  this.attack = function(player){
+    player.health = options.health -10;
+  };
+};
 
 var mario = new Player({
-
+  name: 'Mario',
+  health: 100,
 });
 
 var luigi = new Player({
-
+  name: 'Luigi',
+  health: 100
 });
 
 var peach = new Player({
+  name: 'Peach',
+  health: 100
+});
 
+var bowser = new Enemy({
+  name: 'Bowser',
+  health: 100
+});
+
+var wario = new Enemy({
+  name: 'Wario',
+  health: 100
+});
+
+var waluigi = new Enemy({
+  name: 'Waluigi',
+  health: 100
 });
