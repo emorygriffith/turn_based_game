72bf80c88d52637595f3caa8979a0cfd29e99b24
Emory Griffith <emorygriffith91@gmail.com>
2014-10-22 12:56:58 -0400

    Initial Setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('The Iron Yard Rocks');
+var Player = function (options) {
+  this.name = options.name;
+  this.health = options.health;
+
+};
+
+var Enemy = function (options) {
+  this.name = options.name;
+  this.health = options.health;
+
+}
+
+var mario = new Player({
+
+});
+
+var luigi = new Player({
+
+});
+
+var peach = new Player({
+
+});
